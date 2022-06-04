1c24c4e3dd9fe32e0baa9c93c8bec3095ca9c023
Ярослав Казанков <kazankob07@list.ru>
2022-06-04 12:11:19 +0300

    1

--- userservice.py
+++ userservice.py
@@ -2,19 +2,16 @@ import requests
 import os
 import json
 import configparser
-import time
-from tqdm import tqdm
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
 
 TOKEN_VK = config['vk_api']['access_token']
-TOKEN_YADISK = config['yadisk_api']['api_token']
 
 
 class UserService:
 
-    def __init__(self, user_id, token: str):
+    def __init__(self, user_id, token=str):
         self.user_id = user_id
         self.token = token
         self.api_version = config['vk_api']['api_version']
@@ -22,7 +19,6 @@ class UserService:
         self.download_file_path = config['files_path']['download_file_path']
         self.get_upload_url_api = config['yadisk_api']['get_upload_url_api']
         self.file_path = config['files_path']['download_file_path']
-        self.mkdir_url = config['yadisk_api']['mkdir_url']
 
     def _get_photos_from_folder(self) -> list:
         file_list = os.listdir(self.file_path)
@@ -39,8 +35,7 @@ class UserService:
         response = requests.get(self.get_photos_method_url, params=params)
         profile_list = response.json()
 
-        for file in tqdm(profile_list['response']['items']):
-            time.sleep(3)
+        for file in profile_list['response']['items']:
             self.size = file['sizes'][-1]['type']
             photo_url = file['sizes'][-1]['src']
             file_name = file['likes']['count']
@@ -48,20 +43,10 @@ class UserService:
             with open(f'{self.download_file_path}/{file_name}.jpg', 'wb') as f:
                 f.write(download_photo.content)
 
-    def create_folder(self):
-        # self.mkdir_path = mkdir_path
-        params = {'path': self.file_path}
-        headers = {'Content-Type': 'application/json',
-                   'Authorization': TOKEN_YADISK}
-        create_dir = requests.api.put(self.mkdir_url, headers=headers, params=params)
-
     def upload_photo(self):
         headers = {'Content-Type': 'application/json',
-                   'Authorization': TOKEN_YADISK}
-        logs_list = []
-
-        for photo in tqdm(self._get_photos_from_folder()):
-            time.sleep(3)
+                   'Authorization': config['yadisk_api']['api_token']}
+        for photo in self._get_photos_from_folder():
             params = {'path': f'{self.file_path}/{photo}'}
             get_upload_url = requests.get(self.get_upload_url_api, headers=headers, params=params)
             get_url = get_upload_url.json()
@@ -70,10 +55,8 @@ class UserService:
             status = file_upload.status_code
 
             download_log = {'file_name': photo, 'size': self.size}
-            logs_list.append(download_log)
-
-        with open('files/log.json', 'a') as file:
-            json.dump(logs_list, file, indent=2)
+            with open('files/log.json', 'a') as photo:
+                print(download_log, file=photo)
 
         if 500 > status != 400:
             print('Фотографии успешно загружены!')
@@ -84,5 +67,4 @@ class UserService:
 if __name__ == '__main__':
     user1 = UserService(17198266, TOKEN_VK)
     save_photos = user1.get_photos_method(user1.user_id)
-    user1.create_folder()
     back_up = user1.upload_photo()
